75dfc64922d6d2f50dfae0788513bca74143d28a
gimre <gimre@symbol.dev>
2022-11-01 21:27:06 +0100

    we're using this everywhere

--- recipes/openssl/3.x.x/conanfile.py
+++ recipes/openssl/3.x.x/conanfile.py
@@ -105,6 +105,9 @@ class OpenSSLConan(ConanFile):
             del self.options.capieng_dialog
             del self.options.enable_capieng
         else:
+            if self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version.value) < 15:
+                raise ConanInvalidConfiguration("{} {}, 'Symbol' packages do not support Visual Studio < 15".format(self.name, self.version))
+
             del self.options.fPIC
 
         if self.settings.os == "Emscripten":
