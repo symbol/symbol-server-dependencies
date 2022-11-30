b27ffcb196b04021500787022f4807e5a7afd173
Wayonb <wayon@symbol.dev>
2022-11-29 23:09:12 -0500

    [deps] fix: update deps
    Update the following deps:
    
    1. rocksdb to 7.7.3
    2. benchmark to 1.7.1
    3. mongo-c-driver to 1.23.1
    4. mongo-cxx-driver to 3.7.0
    5. cppzmq to 4.9.0

--- recipes/mongo-c-driver/all/conanfile.py
+++ recipes/mongo-c-driver/all/conanfile.py
@@ -27,7 +27,7 @@ class MongoCDriverConan(ConanFile):
 
     def requirements(self):
         if self.settings.os == "Linux":
-            self.requires("openssl/1.1.1q@nemtech/stable")
+            self.requires("openssl/3.0.7")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
