4072cf5076c1097ff2383c16a2b045ceda1b49f2
wayonb <wayon@symbol.dev>
2025-02-18 13:46:31 -0500

    [recipe]: bump openssl version

--- recipes/mongo-c-driver/all/conanfile.py
+++ recipes/mongo-c-driver/all/conanfile.py
@@ -27,7 +27,7 @@ class MongoCDriverConan(ConanFile):
 
 	def requirements(self):
 		if self.settings.os == "Linux":
-			self.requires("openssl/[^3.3]")
+			self.requires("openssl/[^3.4]")
 
 	def layout(self):
 		cmake_layout(self, src_folder="src")
