6aa7e1826e328ed2cb1078eb6904d486504f5826
wayonb <wayon@symbol.dev>
2025-04-17 15:32:42 -0400

    [recipes] fix: build fail to find openssl problem: latest version of conan as new download url          conan now only checks remotes specify on the comnand line. solution: update to the latest conan version           remove the remote param from the conan create

--- scripts/CatapultRecipeUpdater.py
+++ scripts/CatapultRecipeUpdater.py
@@ -157,7 +157,7 @@ class CatapultRecipesUpdater:
 	async def build_conan_package(self, recipes_versions):
 		await self._execute_conan_package_command(
 			recipes_versions,
-			lambda version, recipe_name: ['conan', 'create', '.', f'--name={recipe_name}', f'--version={version}', '--user=nemtech', '--channel=stable', '--build=missing', '--remote=nemtech']
+			lambda version, recipe_name: ['conan', 'create', '.', f'--name={recipe_name}', f'--version={version}', '--user=nemtech', '--channel=stable', '--build=missing']
 		)
 
 	async def upload_conan_package(self, recipes_versions):
