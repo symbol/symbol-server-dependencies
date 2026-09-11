616e82e42f6757452073f33e06b974cf31ba9292
Wayon <wayon@symbolsyndicate.us>
2026-09-10 21:27:36 -0400

    [recipe]: build all with C++20 std

--- scripts/CatapultRecipeUpdater.py
+++ scripts/CatapultRecipeUpdater.py
@@ -158,8 +158,8 @@ class CatapultRecipesUpdater:
 		await self._execute_conan_package_command(
 			recipes_versions,
 			lambda version, recipe_name: [
-				'conan', 'create', '.', f'--name={recipe_name}', f'--version={version}', '--user=nemtech', '--channel=stable', '--build=missing'
-			] + (['-s=compiler.cppstd=20'] if 'rocksdb' == recipe_name else [])
+				'conan', 'create', '.', f'--name={recipe_name}', f'--version={version}', '--user=nemtech', '--channel=stable', '--build=missing', '-s=compiler.cppstd=20'
+			]
 		)
 
 	async def upload_conan_package(self, recipes_versions):
